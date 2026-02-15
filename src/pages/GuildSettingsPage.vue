6f5b62aeb08765ad48c32cbcf9e9c664312a3dca
Hayato <hayato.fukami.0808@gmail.com>
2026-02-15 16:51:41 +0900

    Create `GuildSettingsPage.vue` with basic structure and props

---
+++ src/pages/GuildSettingsPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HeaderBar from "@/components/HeaderBar.vue";
+
+const props = defineProps({
+  id: { type: String, required: true },
+});
+</script>
+
+<template>
+  <div class="page">
+    <HeaderBar brand-name="Sumire Vox Bot" />
+
+    <div style="padding: 24px;">
+      <h1>Guild Settings</h1>
+      <p>guild id: {{ props.id }}</p>
+    </div>
+  </div>
+</template>

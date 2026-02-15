6bf7f4f942e0ebb9594eace406a979d7607917b8
Hayato <hayato.fukami.0808@gmail.com>
2026-02-15 16:51:55 +0900

    Create `NotFoundPage.vue` with basic structure and 404 message

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HeaderBar from "@/components/HeaderBar.vue";
+</script>
+
+<template>
+  <div class="page">
+    <HeaderBar brand-name="Sumire Vox Bot" />
+
+    <div style="padding: 24px;">
+      <h1>404</h1>
+      <p>ページが見つかりません。</p>
+    </div>
+  </div>
+</template>

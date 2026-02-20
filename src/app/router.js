838410a6bc429f7a3954f2cb8caec717e5d7346b
Hayato <hayato.fukami.0808@gmail.com>
2026-02-20 23:09:11 +0900

    Add route for `PremiumDetailsPage` and scroll behavior configuration in router

--- src/app/router.js
+++ src/app/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
 import DashboardPage from "@/pages/DashboardPage.vue";
 import PremiumPage from "@/pages/PremiumPage.vue";
+import PremiumDetailsPage from "@/pages/PremiumDetailsPage.vue";
 import FeaturesPage from "@/pages/FeaturesPage.vue";
 import GuildSettingsPage from "@/pages/GuildSettingsPage.vue";
 import NotFoundPage from "@/pages/NotFoundPage.vue";
@@ -14,7 +15,15 @@ export const router = createRouter({
         { path: "/features", component: FeaturesPage },
         { path: "/dashboard", component: DashboardPage },
         { path: "/dashboard/premium", component: PremiumPage },
+        { path: "/premium", component: PremiumDetailsPage },
         { path: "/dashboard/guilds/:id", component: GuildSettingsPage, props: true },
         { path: "/:pathMatch(.*)*", component: NotFoundPage },
     ],
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0 };
+        }
+    },
 });
